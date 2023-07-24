9ff603a78f0e7815ebf0ef72c6bc7e470d5632b3
coastman <289530309@qq.com>
2023-07-24 19:53:18 +0800

    feat: cache 配置写入配置文件 & 验证 rsa 加密的有效性

--- src/modules/cache/cache.module.ts
+++ src/modules/cache/cache.module.ts
@@ -1,19 +1,24 @@
 import { Module, CacheModule } from '@nestjs/common';
 import { redisStore } from 'cache-manager-redis-store';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    CacheModule.register({
+    CacheModule.registerAsync({
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      store: async () =>
-        await redisStore({
-          socket: {
-            host: 'localhost',
-            port: 6379,
-          },
-        }),
+      useFactory: async (configService: ConfigService) => {
+        return {
+          store: await redisStore({
+            socket: {
+              host: configService.get<string>('REDIS_HOST'),
+              port: configService.get<number>('REDIS_PORT'),
+            },
+          }),
+        };
+      },
       isGlobal: true,
+      inject: [ConfigService],
     }),
   ],
   providers: [],
--- src/modules/rsa/rsa.controller.ts
+++ src/modules/rsa/rsa.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const NodeRSA = require('node-rsa');
@@ -8,8 +8,9 @@ export class RsaController {
   dencrypt: any;
   constructor(private readonly configService: ConfigService) {
     this.dencrypt = new NodeRSA(this.configService.get<string>('PRIVATE_KEY'));
+    this.dencrypt.setOptions({ encryptionScheme: 'pkcs1' });
     // const text = this.dencrypt.decrypt(
-    //   'hpDNTv85YSuk1pgqzlUT4RkzRIMAjmqjrkFv0gO6dFCLpGboLlxYnJYBjLHaeVzzNK9HaZv4f6aGI45FaLNWLA==',
+    //   'BumRdDljHxsuuI1bVZGnALFc8GEztd1iCviZIgXVgfprvtAPHB3j1gmkubZizNqvx2txUMLQtF2TRGEDDvYUvg==',
     //   'utf8',
     // );
     // console.log('text: --->', text);
@@ -18,5 +19,13 @@ export class RsaController {
   @Get()
   async getPublicKey() {
     return this.configService.get<string>('PUBLIC_KEY');
+  }
+
+  @Post()
+  async login(@Body() body: any) {
+    console.log('password: --->', body.password);
+    const text = this.dencrypt.decrypt(body.password, 'utf8');
+    console.log('text: --->', text);
+    return {};
   }
 }
